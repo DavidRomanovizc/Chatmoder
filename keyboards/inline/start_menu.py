a97f52aba2c28e172e2c4c04e69d2621ab866c85
DavidRomanov <72649244+DavidRomanovizc@users.noreply.github.com>
2021-01-28 21:41:29 +0300

    Update start_menu.py

--- keyboards/inline/start_menu.py
+++ keyboards/inline/start_menu.py
@@ -1,7 +1,5 @@
 from aiogram import types
 
-"""Клавиатура, которая используется 
-при стартовом сообщении. (!/start)"""
 
 # Создаем клавиатуру
 start_markup = types.InlineKeyboardMarkup(row_width=3)
@@ -17,12 +15,12 @@ start_markup.row(*row_btns)
 
 # Добавляем обычные кнопки
 start_markup.add(
-    types.InlineKeyboardButton('Мои исходники', url='https://github.com/Latand/CommonChatModer'),
-    types.InlineKeyboardButton('Чатик', url='https://t.me/bot_devs_novice'),
+    types.InlineKeyboardButton('Мой github', url='https://github.com/DavidRomanovizc'),
+    types.InlineKeyboardButton('Чат', url='https://t.me/joinchat/GhZKbBi0KvGAlrMZ'),
 )
 
 
 source_markup = types.InlineKeyboardMarkup()
 source_markup.insert(
-    types.InlineKeyboardButton('Исходники бота', url='https://github.com/Latand/CommonChatModer'),
+    types.InlineKeyboardButton('Бот', url='https://github.com/DavidRomanovizc/Chatmoder'),
 )
